8f3f407e0473b095144e929ecb1da154ba1bc195
BartlomiejRyczek <bryczek221@gmail.com>
2025-01-08 22:10:25 +0100

    Wersja z 08.01.2025

--- scraper_just_join.py
+++ scraper_just_join.py
@@ -1,16 +1,106 @@
 from bs4 import BeautifulSoup
 import csv
 import os
-from datetime import datetime
-import requests
+from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
-url = "https://justjoin.it/job-offers/all-locations/python?with-salary=yes&orderBy=DESC&sortBy=newest"
-headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-    }
-response = requests.get(url, headers=headers)
-response.raise_for_status()
-soup = BeautifulSoup(response.content, 'html.parser')
+driver = webdriver.Chrome()
+driver.get('https://justjoin.it/job-offers/all-locations?with-salary=yes&orderBy=DESC&sortBy=newest')
+body = driver.find_element(By.TAG_NAME, "body")
+for _ in range(15):  # Przewiń stronę kilka razy
+    body.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.4)  # Czekaj chwilę na załadowanie nowych danych
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+driver.quit()
 
-with open('parsed_page_just_join.html', 'w', encoding='utf-8') as file:
-            file.write(soup.prettify())
+csv_file_path = 'job_offers_just_join.csv'
+existing_records = set()
+
+# Sprawdzenie, czy plik CSV istnieje i wczytanie istniejących rekordów
+if os.path.exists(csv_file_path):
+    with open(csv_file_path, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)  # Pominięcie nagłówka
+        for row in reader:
+            if len(row) >= 5:  # Sprawdzamy kompletność wierszy
+                existing_records.add((row[0], row[1], row[5]))  # Tytuł, Firma, Link
+
+# Wyszukiwanie ofert pracy
+offers = soup.find_all('div', class_='MuiBox-root css-ai36e1') 
+
+# Debugowanie liczby znalezionych ofert
+print(f"Liczba znalezionych ofert: {len(offers)}")
+
+new_data = []
+
+for offer in offers:
+    # Pobranie tytułu oferty
+    title_tag = offer.find('h3', class_='css-1gehlh0')
+    title = title_tag.get_text(strip=True) if title_tag else 'Brak tytułu'
+
+    # Pobranie nazwy firmy
+    company_tag = offer.find('div', class_='MuiBox-root css-1kb0cuq')
+    if company_tag:
+        span = company_tag.find('span')
+        company_name = span.get_text(strip=True)
+    else:
+        company_name = "Brak nazwy firmy"
+
+    # Pobranie daty opublikowania
+    date_tag = offer.find('div', class_='MuiBox-root css-jikuwi')
+    if date_tag:
+        date_text = date_tag.get_text(strip=True)
+        if "New" in date_text:
+            date = datetime.now().strftime('%Y-%m-%d')
+        else:
+            date = date_text
+            if "d ago" in date:
+                days_ago = int(date.replace("d ago", "").strip())
+                result_date = datetime.today() - timedelta(days=days_ago)
+                date = result_date.strftime('%Y-%m-%d')
+    else:
+        date = 'Brak daty'
+
+    # Pobranie wynagrodzenia
+    salary_tag = offer.find('div', class_='MuiBox-root css-18ypp16')
+    if salary_tag:
+        salary_spans = salary_tag.find_all('span')
+        salary_min = salary_spans[0].get_text(strip=True)
+        salary_max = salary_spans[1].get_text(strip=True)
+        currency = salary_spans[2].get_text(strip=True)
+        salary = f"{salary_min} - {salary_max} {currency}"
+    else:
+        salary = 'Brak wynagrodzenia'
+        
+    # technologie wymagane przez pracodawcę
+    tags_1 = offer.find_all('div', class_='MuiBox-root css-jikuwi')
+    technologies = [element.get_text(strip=True) for element in tags_1]
+    technologies = technologies[1:]
+
+    # Pobranie linku do oferty
+    link_tag = offer.find('a', class_='offer_list_offer_link')
+    link = 'https://justjoin.it' + link_tag['href'] if link_tag else 'Brak linku'
+
+    # Dodanie danych do new_data
+    record = (title, company_name, link)
+    if record not in existing_records:
+        new_data.append([title, company_name, date, salary, technologies, link])
+        existing_records.add(record)
+    else:
+        print(f"Rekord już istnieje: {record}")
+
+print(f"Ścieżka do pliku CSV: {csv_file_path}")
+print(f"Nowe dane do zapisania: {new_data}")
+
+with open(csv_file_path, 'a', encoding='utf-8', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    # Dodanie nagłówka tylko w przypadku tworzenia nowego pliku
+    if os.stat(csv_file_path).st_size == 0:
+        csvwriter.writerow(['Tytuł', 'Firma', 'Data opublikowania', 'Wynagrodzenie', 'Technologie', 'Link'])
+    csvwriter.writerows(new_data)
+
+print(f"Dodano {len(new_data)} nowych rekordów do pliku job_offers_just_join.csv.")
--- scraper_pracuj_pl.py
+++ scraper_pracuj_pl.py
@@ -1,19 +1,22 @@
 from bs4 import BeautifulSoup
 import csv
 import os
-from datetime import datetime
-import requests
+from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
-url = "https://it.pracuj.pl/praca?et=1%2C3%2C17&itth=37"
-headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-    }
-response = requests.get(url, headers=headers)
-response.raise_for_status()
-soup = BeautifulSoup(response.content, 'html.parser')
-
-with open('parsed_page.html', 'w', encoding='utf-8') as file:
-            file.write(soup.prettify())
+url = "https://it.pracuj.pl/praca?et=1%2C3%2C17"
+driver = webdriver.Chrome()
+driver.get(url)
+body = driver.find_element(By.TAG_NAME, "body")
+for _ in range(15):  # Przewiń stronę kilka razy
+    body.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.2)  # Czekaj chwilę na załadowanie nowych danych
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+driver.quit()
 
 # Funkcja do konwersji daty na format ISO 8601
 def convert_to_iso(date_str):
@@ -40,51 +43,53 @@ if os.path.exists(csv_file_path):
         next(reader, None)  # Pominięcie nagłówka
         for row in reader:
             if len(row) >= 5:  # Sprawdzamy kompletność wierszy
-                existing_records.add((row[0], row[1], row[4]))  # Tytuł, Firma, Link
-
-# Wczytanie pliku HTML
-with open('parsed_page.html', 'r', encoding='utf-8') as file:
-    soup = BeautifulSoup(file, 'html.parser')
+                existing_records.add((row[0], row[1], row[5]))  # Tytuł, Firma, Link
 
 # Przygotowanie listy na nowe dane
 new_data = []
 
 # Wyszukiwanie ofert pracy
-offers = soup.find_all('div', class_='tiles_cobg3mp')  # Klasa grupująca każdą ofertę pracy
+offers = soup.find_all('div', class_='gp-pp-reset tiles_b18pwp01 core_po9665q')  # Klasa grupująca każdą ofertę pracy
 
 for offer in offers:
     # Pobranie tytułu oferty
-    title_tag = offer.find('h2', class_='tiles_h1p4o5k6')
+    title_tag = offer.find('a', class_='tiles_o1859gd9 core_n194fgoq')
     title = title_tag.get_text(strip=True) if title_tag else 'Brak tytułu'
 
     # Pobranie nazwy firmy
-    company_tag = offer.find('h3', class_='tiles_chl8gsf')
+    company_tag = offer.find('h3', class_='tiles_chl8gsf size-caption core_t1rst47b')
     company = company_tag.get_text(strip=True) if company_tag else 'Brak nazwy firmy'
 
     # Pobranie daty opublikowania i konwersja na format ISO 8601
-    date_tag = offer.find('p', class_='tiles_arxx4s5')
+    date_tag = offer.find('p', class_='tiles_a1nm2ekh tiles_s1pgzmte tiles_bg8mbli core_pk4iags size-caption core_t1rst47b')
     date = convert_to_iso(date_tag.get_text(strip=True).replace('Opublikowana:', '').strip()) if date_tag else 'Brak daty'
 
     # Pobranie wynagrodzenia
     salary_tag = offer.find('span', class_='tiles_s1x1fda3')
     salary = salary_tag.get_text(strip=True).replace(' zł / mies. (zal. od umowy)', '') if salary_tag else 'Brak wynagrodzenia'
 
+    # Pobieranie wymaganej znajomosci technologii
+    tech_tag = offer.find_all('span', class_='_chip_hmm6b_1 _chip--highlight_hmm6b_1 _chip--small_hmm6b_1 _chip--full-corner_hmm6b_1 tiles_c276mrm')
+    technologies = [tag.get_text(strip=True) for tag in tech_tag] if tech_tag else 'Brak technologii'
     # Pobranie linku do oferty
-    link_tag = offer.find('a', class_='tiles_o1859gd9')
+    link_tag = offer.find('a', class_='tiles_cnb3rfy core_n194fgoq')
     link = link_tag['href'] if link_tag else 'Brak linku'
 
-    # Sprawdzenie, czy oferta już istnieje w pliku
+# Sprawdzenie, czy oferta już istnieje w pliku
     record = (title, company, link)
     if record not in existing_records:
-        new_data.append([title, company, date, salary, link])
-        existing_records.add(record)  # Dodaj do zbioru, aby uniknąć duplikatów
+        new_data.append([title, company, date, salary, technologies, link])
+        existing_records.add(record)
 
 # Zapisanie nowych danych do pliku CSV (dodawanie do istniejących)
+print(f"Ścieżka do pliku CSV: {csv_file_path}")
+print(f"Nowe dane do zapisania: {new_data}")
+
 with open(csv_file_path, 'a', encoding='utf-8', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
     # Dodanie nagłówka tylko w przypadku tworzenia nowego pliku
     if os.stat(csv_file_path).st_size == 0:
-        csvwriter.writerow(['Tytuł', 'Firma', 'Data opublikowania', 'Wynagrodzenie', 'Link'])
+        csvwriter.writerow(['Tytuł', 'Firma', 'Data opublikowania', 'Wynagrodzenie', 'Technologie', 'Link'])
     csvwriter.writerows(new_data)
 
-print("Dane zostały zapisane do pliku job_offers.csv.")
+print(f"Dodano {len(new_data)} nowych rekordów do pliku job_offers.csv.")
